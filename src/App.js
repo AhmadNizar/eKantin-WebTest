a3f15b23e71aaef3bd5c16f4f8cd328f463b1f5d
AhmadNizar <ahmadnizar.owl@gmail.com>
2018-01-30 22:46:27 +0700

    -set search datatable and name sorting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import faker from 'faker'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      initMenus: ['Home', 'About', 'Contact'],
-      menus: ''
+      initMenus: ['About', 'Base', 'Blog', 'Contact', 'Custom', 'Support', 'Tools'],
+      menus: '',
+      initDataTable: [],
+      dataTable: '',
+      nameSortStatus: 'up'
+    }
+  }
+  sortByName = () => {
+    console.log('jos')
+    let tempDataTable = this.state.dataTable
+    // sort by name
+    if(this.state.nameSortStatus === 'down') {
+      tempDataTable.sort(function(a, b) {
+        var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        // names must be equal
+        return 0;
+      });
+
+      this.setState({
+        dataTable: tempDataTable,
+        nameSortStatus: 'up'
+      })
+    }
+
+    if(this.state.nameSortStatus === 'up') {
+      tempDataTable.sort(function(a, b) {
+        var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA > nameB) {
+          return -1;
+        }
+        if (nameA < nameB) {
+          return 1;
+        }
+        // names must be equal
+        return 0;
+      });
+
+      this.setState({
+        dataTable: tempDataTable,
+        nameSortStatus: 'down'
+      })
     }
   }
   dropdownFunction() {
@@ -17,13 +65,26 @@ class App extends Component {
     let userInput = event.target.value
     let inputRegex = new RegExp(userInput.toLowerCase())
     let newMenus = this.state.initMenus.filter(menu => {
-      if(menu.toLowerCase().search(inputRegex) != -1 ) {
+      if(menu.toLowerCase().search(inputRegex) !== -1 ) {
         return menu
       }
     })
 
     this.setState({
       menus : newMenus
+    })
+  }
+  handleChangeDataTable = (event) => {
+    let userInput = event.target.value
+    let inputRegex = new RegExp(userInput.toLowerCase())
+    let newDataTable = this.state.initDataTable.filter(data => {
+      if(data.name.toLowerCase().search(inputRegex) !== -1 ) {
+        return data
+      }
+    })
+
+    this.setState({
+      dataTable : newDataTable
     })
   }
   render() {
@@ -33,24 +94,77 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <div className="dropdown">
           <button onClick={this.dropdownFunction} className="dropbtn">Dropdown</button>
           <div id="myDropdown" className="dropdown-content">
-            <input type="text" name="name" onChange={this.handleChange} />
+            <input type="text" placeholder="Search.." name="name" onChange={this.handleChange} />
             {this.state.menus.map((object, i) => {
               return <a href="#">{object}</a>
             })}
           </div>
         </div>
-      </div>
+        <br/>
+        <br/>
+        <div className="nav-table">
+          <div className="page-table">
+            show
+            <select>
+              <option value="10">10</option>
+              <option value="9">9</option>
+              <option value="8">8</option>
+              <option value="7">7</option>
+              <option value="6">6</option>
+              <option value="5">5</option>
+            </select>
+            entries
+          </div>
+          <div className="search-table">
+            Search: <input type="text" name="username" onChange={this.handleChangeDataTable} />
+          </div>
+        </div>
+        <div className="data-table">
+          <table>
+            <tr>
+              <th onClick={this.sortByName}><a>Name</a></th>
+              <th>Position</th>
+              <th>Office</th>
+              <th>Age</th>
+              <th>Start date</th>
+              <th>Salary</th>
+            </tr>
+            {this.state.dataTable.map((object, i) => {
+              return (
+                <tr>
+                  <td>{object.name}</td>
+                  <td>{object.position}</td>
+                  <td>{object.office}</td>
+                  <td>{object.age}</td>
+                  <td>{object.name}</td>
+                  <td>${object.salary}</td>
+                </tr>
+              )
+            })}
+          </table>
+          </div>
+        </div>
     );
   }
   componentWillMount() {
+    let initDataTable = []
+    for(let i=0; i<10; i++) {
+      initDataTable.push({
+        name: faker.name.findName(),
+        position: faker.name.jobTitle(),
+        office: faker.address.city(),
+        age: Math.floor((Math.random() * 55) + 20),
+        startDate: faker.date.past(),
+        salary: faker.commerce.price()
+      })
+    }
     this.setState({
-      menus: this.state.initMenus
+      menus: this.state.initMenus,
+      initDataTable: initDataTable,
+      dataTable: initDataTable
     })
   }
 }
